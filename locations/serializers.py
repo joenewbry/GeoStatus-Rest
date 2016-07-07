2a2241373c9f6c41c032cdd95ee3eef649139b2f
Joe Newbry <joenewbry@gmail.com>
2016-07-07 14:29:12 -0700

    Finished part 1 of the tutorial

--- locations/serializers.py
+++ locations/serializers.py
@@ -2,24 +2,7 @@ from rest_framework import serializers
 from locations.models import Location, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class LocationSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    latitude = serializers.FloatField(required=True)
-    longitude = serializers.FloatField(required=True)
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Location` instance, given the validated data.
-        """
-        return Location.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Location` instance, given the validated
-        data.
-        """
-        instance.latitude = validated_data.get('latitude', instance.latitude)
-        instance.longitude = validated_data.get('longitude',
-                                                instance.longitude)
-        instance.save()
-        return instance
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ('id', 'latitude', 'longitude')
--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from locations.models import Location
+from locations.serializers import LocationSerializer
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def location_list(request):
+    """
+    List all locations, or create a new location
+    """
+    if request.method == 'GET':
+        locations = Location.objects.all()
+        serializer = LocationSerializer(locations, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = LocationSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+    return JSONResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def location_detail(request, pk):
+    """
+    Retrieve, update, or delete a location.
+    """
+    try:
+        location = Location.objects.get(pk=pk)
+    except Location.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = LocationSerializer(location)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = LocationSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        location.delete()
+        return HttpResponse(status=204)
+
