fd0b07227daa50f271b5f29db4180747418dc197
Joe Newbry <joenewbry@gmail.com>
2016-07-07 14:46:21 -0700

    Some functionality for locations

--- locations/urls.py
+++ locations/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from locations import views
 
 urlpatterns = [
     url(r'^locations/$', views.location_list),
     url(r'^locations/(?P<pk>[0-9]+)/$', views.location_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- locations/views.py
+++ locations/views.py
@@ -1,63 +1,49 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from locations.models import Location
 from locations.serializers import LocationSerializer
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
 
-
-@csrf_exempt
-def location_list(request):
+@api_view(['GET', 'POST'])
+def location_list(request, format=None):
     """
     List all locations, or create a new location
     """
     if request.method == 'GET':
         locations = Location.objects.all()
         serializer = LocationSerializer(locations, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
         serializer = LocationSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-    return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def location_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def location_detail(request, pk, format=None):
     """
     Retrieve, update, or delete a location.
     """
     try:
         location = Location.objects.get(pk=pk)
     except Location.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'GET':
         serializer = LocationSerializer(location)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
         serializer = LocationSerializer(snippet, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         location.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
