c703b83350ae15b7ff6db992e6224f208088975e
Joe Newbry <joenewbry@gmail.com>
2016-07-28 17:18:29 -0700

    Slash command in slack hits /slack/ to get latest stats for a user

--- slack/views.py
+++ slack/views.py
@@ -1,16 +1,50 @@
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
-from locations.models import Status
+from locations.models import GeoStatus
+import requests
 
 # From http://www.django-rest-framework.org/tutorial/2-requests-and-responses/
 # Could use class based views instead
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['POST', 'GET'])
 def user_status(request):
     """
     Get current user status
     """
-    most_recent_status = Status.last().context.name
-    return Response(most_recent_status)
+
+    # TODO: add some basic validation
+    username = request.data['text']
+    geostatus = GeoStatus.objects.filter(username=username).last()
+    if request.method == 'GET':
+    	return Response(geostatus)
+
+
+
+    if geostatus is None:
+        payload = {'text': 'No GeoStatus found for ' + request.data['text']}
+        return Response(payload)
+    else:
+        username = geostatus.username
+        verb = geostatus.verb
+        location = geostatus.location
+        text_response = "" + username + " " + verb + " " + location
+        payload = {'text': text_response}
+        return Response(payload)
+
+"""
+Data posted by slack to external url
+------------------------------------
+token=gIkuvaNzQIHg97ATvDxqgjtO
+team_id=T0001
+team_domain=example
+channel_id=C2147483705
+channel_name=test
+user_id=U2147483697
+user_name=Steve
+command=/weather
+text=94070
+response_url=https://hooks.slack.com/commands/1234/5678
+"""
